e2feecacbed2e284f865004921604e8fd68e2f19
IfrimAlexandru <ifrimalexandru27@gmail.com>
2024-05-14 20:03:32 +0200

    aggiornamento cartelle

---
+++ BackEnd/UNIT2/Settimana1/EsercizioS1G2/src/main/java/it/nextdevs/EsercizioS1G2/EsercizioS1G2Application.java
@@ -0,0 +1,13 @@
+package it.nextdevs.EsercizioS1G2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioS1G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioS1G2Application.class, args);
+	}
+
+}
---
+++ BackEnd/UNIT2/Settimana1/EsercizioS1G2/src/test/java/it/nextdevs/EsercizioS1G2/EsercizioS1G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nextdevs.EsercizioS1G2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioS1G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
